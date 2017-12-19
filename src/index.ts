4af2e5f98f1ed33db3b5943b2854a577cc864496
Miguel Gil Rosas <miguelgilrosas@gmail.com>
2017-12-19 16:42:16 +0000

    Pequeños cambios no esenciales

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
+let segundosAnteriores = 61
+const reloj = document.getElementById('reloj')
+requestAnimationFrame(animaReloj)
+
 function animaReloj(): void {
-    let ahora: Date = new Date()
-    let segundos = ahora.getSeconds()
+    const ahora: Date = new Date()
+    const segundos = ahora.getSeconds()
     if (segundosAnteriores !== segundos) {
         segundosAnteriores = segundos
-        let minutos = ahora.getMinutes()
-        let horas = ahora.getHours() % 12
+        const minutos = ahora.getMinutes() + segundos / 60
+        const horas = ahora.getHours() % 12 + minutos / 60
         reloj.style.setProperty('--segundoAng', (6 * segundos + 270) % 360 + 'deg')
-        minutos += segundos / 60
         reloj.style.setProperty('--minutoAng', (6 * minutos + 270) % 360 + 'deg')
-        horas += minutos / 60
         reloj.style.setProperty('--horaAng', (30 * horas + 270) % 360 + 'deg')
     }
     requestAnimationFrame(animaReloj)
 }
-
-let segundosAnteriores = 61
-let reloj = document.getElementById('reloj')
-requestAnimationFrame(animaReloj)
